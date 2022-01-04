6f1215db572541cbd93e60a17e9fc93031507638
notgarrett <Gnome9999>
2022-01-04 13:14:51 -0500

    General update.

--- src/commands/Guild/ban.ts
+++ src/commands/Guild/ban.ts
@@ -1,13 +1,19 @@
 import { Command } from '../../interfaces';
-import { Users } from '../../functions/usersObject';
 import { RAPI } from '../../functions';
 import { Embeds } from '../../embeds/embeds';
+import { checkBotAdmin } from '../../functions/commandAdmin';
 
 export const command: Command = {
   name: 'ban',
   aliases: ['b'],
   run: async (client, message, args) => {
     const channelMessage = new Embeds(message.channel);
+
+    if (!checkBotAdmin(message))
+      return channelMessage.failure(
+        'Missing Permissions',
+        'You do not have permission to use that command.'
+      );
 
     console.log(args[0]);
     if (!args[0]) {
