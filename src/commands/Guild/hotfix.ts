8a06e99f390570a224cbc9083d89eb270e5e3d07
notgarrett <Gnome9999>
2022-02-09 00:09:28 -0500

    Final push.

--- src/commands/Guild/hotfix.ts
+++ src/commands/Guild/hotfix.ts
@@ -2,7 +2,7 @@ import { Command } from '../../interfaces';
 import { model } from 'mongoose';
 import { User } from '../../interfaces';
 import { userModel } from '../../models';
-import { RAPI } from '../../functions';
+import { Embeds } from '../../embeds/embeds';
 import { securityCheck } from '../../functions/commandAdmin';
 
 const UserModel = model<User>('User', userModel);
@@ -17,10 +17,13 @@ export const command: Command = {
   name: 'hotfix',
   aliases: [''],
   run: async (client, message) => {
+    const serverResponse = new Embeds(message.channel);
     if (!(await securityCheck(message, 3))) return;
 
+    serverResponse.notification('Process started.', 'Give us a moment.');
     const docs = await UserModel.find({});
     console.log(docs.length);
+    let count = 0;
     for (let i in docs) {
       console.log(i);
       const profile = docs[i];
@@ -36,8 +39,11 @@ export const command: Command = {
         console.log(profile.RobloxUserName + ' Exists!');
       } else {
         console.log(profile.RobloxUserName + ' Does not!');
-        //await UserModel.deleteOne({ DiscordId: discordId });
+        count++;
+        await UserModel.deleteOne({ DiscordId: discordId });
       }
+
+      serverResponse.success('Process finished.', `${count} users wiped.`);
     }
   },
 };
